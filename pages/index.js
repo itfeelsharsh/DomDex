d2c9bcbdcabccc89dd547b73780779b953ec1f1b
itfeelsharsh <itfeelsharsh@gmail.com>
2022-08-22 18:56:46 +0530

    first commit

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>DOMDEX - the shop</title>
+        <meta name="description" content="A complete shopping web-app" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+ <div className="mx-4">This is me</div>
+
+    </div>
+  )
+}
