02559cd59ff5ea1db5f7d9803b3ab809c55b6e50
itfeelsharsh <itfeelsharsh@gmail.com>
2022-08-24 15:01:25 +0530

    new home page

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,29 +5,29 @@ import { BsCart3 } from 'react-icons/Bs';
 
 const Navbar = () => {
   return (
-    <div>
-        <div className='flex flex-col md:flex-row md:justify-start justify-between items-center py-2 shadow-xl' >
-            <div className="logo mx-50">
-           <Link href={'/'}><a><Image src="/DomDex.png" alt="logo" width={99} height={99} /></a></Link>
-            </div>
-            <div className="nav">
-                <ul className="flex items-center space-x-2 font-bold md:text-x">
-                    <div>&nbsp;&nbsp;&nbsp;</div>
-                    <div className='text-sm sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4' >
-                    <Link href={'/about'} ><a><li>About</li> </a></Link> </div>
-                    <div className='text-sm sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4' >
-                    <Link href={'/contact'} ><a><li>Contact</li> </a></Link></div>
-                    <div className='text-sm sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4' >
-                    <Link href={'/products'} ><a><li>Products</li> </a></Link></div>
-                    <div className='text-sm sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4' >&nbsp;</div>
-                   
-                </ul>
-            </div>
-            <div className="BsCart3 absolute right-0 top-4  mx-5">
+<header className="text-gray-400 bg-gray-900 body-font justify-center ">
+  <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+    <a className="flex title-font font-medium items-center text-white mb-4 md:mb-0">
+    <div className="cursor-pointer">
+      <Link href={'/'}><span className="ml-3 text-xl">DOMDEX</span></Link>
+      </div>
+    </a>    
+    <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center cursor-pointer">
+        <Link href={'/about'} ><a className="mr-5 hover:text-white">About</a></Link>
+        <Link href={'/contact'} ><a className="mr-5 hover:text-white">Contact</a></Link>
+        <Link href={'/products'} ><a className="mr-5 hover:text-white">Products</a></Link>
+        
+    
+    </nav>
+
+    <div className="BsCart3 absolute right-0 top-4  mx-5 cursor-pointer ">
                 <BsCart3 className='text-4xl'/>
-            </div>
-        </div>
     </div>
+
+
+
+  </div>
+</header>
   )
 }
 
